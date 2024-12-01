97c86c70f80222ccf615b7bcfd12930186859bb1
DEEKKdk <ytstatuswale@gmail.com>
2024-12-01 16:30:32 +0530

    Add files via upload

---
+++ deekk.js
@@ -0,0 +1,5 @@
+// Add interactivity here if needed
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Thank you for reaching out! I will get back to you soon.');
+});
